837091ffe3e41fd5e421a0c606ed3b46545cdbe5
Logann Johnson <02logann.johnson@gulllakecs.org>
2021-10-13 09:49:52 -0700

    Add First & Last Name Variables
    We declared our First & Last name variables in our program and tested if they worked in a Print statement by concatenated the two variables.

--- Control Flow.py
+++ Control Flow.py
@@ -2,3 +2,16 @@
 #Date: 10.11.21
 #Program: Control Flow
 
+"""
+This program simulates an ATM utilizing If, Elis, & Else statements
+Nesting If statements and refresh our Comparison & Logical Operators
+"""
+
+print("Welcome to Cash-R-Us Bank\nLet's take a moment to set up your account.\n")
+
+# Set up account by asking users for their First & Last name using Variables
+
+firstName = input("What is your first name: ")
+lastName = input("What is your last name: ")
+
+print("\nWelcome to Cash-R-Us", firstName,lastName + ", we will now set up a security PIN on your account. \n")
