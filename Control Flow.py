938d8a4d07bab0da8b76b52a601d2a2321856124
Logann Johnson <02logann.johnson@gulllakecs.org>
2021-10-14 09:53:46 -0700

    We created a PIN variable

--- Control Flow.py
+++ Control Flow.py
@@ -1,6 +1,6 @@
 #Developer: Logann Johnson
 #Date: 10.11.21
-#Program: Control Flow
+#Program: ATM Bank Transaction
 
 """
 This program simulates an ATM utilizing If, Elis, & Else statements
@@ -15,3 +15,8 @@ firstName = input("What is your first name: ")
 lastName = input("What is your last name: ")
 
 print("\nWelcome to Cash-R-Us", firstName,lastName + ", we will now set up a security PIN on your account. \n")
+
+# Set up a PIN - Personal Identification Number
+pin = input("Please choose a 4 digit Personal Identification Number: ")
+
+print("\nThank you",firstName + ", we see that you set your PIN to",pin)
